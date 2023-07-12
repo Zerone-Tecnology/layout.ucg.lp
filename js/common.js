bbfe661a58bddd27165539e056f0b063de2e9182
Damir Rashidinov <damir.dmr88@gmail.com>
2023-07-12 11:37:01 +0600

    mobile view programm

--- js/common.js
+++ js/common.js
@@ -67,34 +67,38 @@ $(function() {
 		});
 	}
 
-	var countDownDate = new Date("Sep 18, 2023 09:00:00").getTime();
+	//timer
+	var timerDiv = $('.timer').length;
+	if(timerDiv != 0){
+		var countDownDate = new Date("Sep 18, 2023 09:00:00").getTime();
 
-	var x = setInterval(function() {
+		var x = setInterval(function() {
 
-		// Get today's date and time
-		var now = new Date().getTime();
-	
-		// Find the distance between now and the count down date
-		var distance = countDownDate - now;
-	
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	
-		// Display the result in the element with id="demo"
-		document.getElementById("tday").innerHTML = days;
-		document.getElementById("thours").innerHTML = hours;
-		document.getElementById("tminutes").innerHTML = minutes;
-		document.getElementById("tseconds").innerHTML = seconds;
-	
-		// If the count down is finished, write some text
-		if (distance < 0) {
-			clearInterval(x);
-			document.getElementById("timer").innerHTML = "EXPIRED";
-		}
-	}, 1000);
+			// Get today's date and time
+			var now = new Date().getTime();
+		
+			// Find the distance between now and the count down date
+			var distance = countDownDate - now;
+		
+			// Time calculations for days, hours, minutes and seconds
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		
+			// Display the result in the element with id="demo"
+			document.getElementById("tday").innerHTML = days;
+			document.getElementById("thours").innerHTML = hours;
+			document.getElementById("tminutes").innerHTML = minutes;
+			document.getElementById("tseconds").innerHTML = seconds;
+		
+			// If the count down is finished, write some text
+			if (distance < 0) {
+				clearInterval(x);
+				document.getElementById("timer").innerHTML = "EXPIRED";
+			}
+		}, 1000);
+	}
 
 	$('.gallery-items-first').addClass("owl-carousel");
 	$('.gallery-items-second').addClass("owl-carousel");
